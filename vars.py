873da3347d3b9118bacfc4f2233ddf26624a7b73
anuuyadav885 <167565005+anuuyadav885@users.noreply.github.com>
2024-04-28 12:17:05 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
 api_id = "27464924"
 api_hash = "f28d089001481597f045b70a0daa73e4"
-bot_token = "6512589605:AAHrgnMZrlu3OIqjMeOEt1I1jugEKWq7Kn8"
-auth_groups = "-1002082127207"
+bot_token = "6926246475:AAGtBESgyy6OtLsnsi0VQNWRMWeTBAxBSfI"
+auth_groups = "-1002125995517"
